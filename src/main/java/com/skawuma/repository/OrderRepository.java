6117e6a007b03ff0d9d8fd1d6994e1ce516e8777
skawuma <82976186+skawuma@users.noreply.github.com>
2023-11-25 13:11:56 -0500

    tryout

--- src/main/java/com/skawuma/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.skawuma.repository;
-
-import com.skawuma.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
